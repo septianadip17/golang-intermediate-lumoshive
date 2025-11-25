eb2eaca5c27249568b0f2ad35f95e6f4095d856a
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-25 11:55:14 +0700

    set up main

---
+++ unit-test/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// Add menjumlahkan dua bilangan dan mengembalikan hasilnya
+func Add(a, b int) int {
+	return a + b
+}
